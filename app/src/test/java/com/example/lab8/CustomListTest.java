b92fa4cd9b7440c7d2caaac0d94dc928a7b5a640
“Jessica <emereony@ualberta.ca>
2023-03-07 17:48:49 -0700

    RED: test adding city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,10 +1,10 @@
 package com.example.lab8;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +12,15 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private ArrayList<City> citylist = new ArrayList<>();
+
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+
+    }
 
 }
